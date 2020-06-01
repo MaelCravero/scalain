bcfa16e5f19c6c1d3e60e2509035d3efe0141439
Mael Cravero <mael.cravero@epita.fr>
2020-06-01 19:03:36 +0200

    Update Database start method
    Start now returns a squeryl Session object to make sure that the
    connection to the DB will only be closed manually.

--- src/main/scala/database/Database.scala
+++ src/main/scala/database/Database.scala
@@ -9,15 +9,26 @@ import org.squeryl.adapters.H2Adapter
 object Database {
   Class.forName("org.h2.Driver")
 
-  val url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
-  val connection = java.sql.DriverManager.getConnection(url)
+  private val url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
+  private val connection = java.sql.DriverManager.getConnection(url)
 
-  def start() {
-    SessionFactory.concreteFactory =
-      Some(() => Session.create(connection, new H2Adapter))
+  def start(): org.squeryl.Session = {
+    val session = Session.create(connection, new H2Adapter)
+
+    using(session) {
+      Tables.create
+    }
+
+    session
   }
 
   object Tables extends Schema {
     val countries = table[Country]("COUNTRIES")
+
+    def dump(implicit session: org.squeryl.Session) =
+      using(session) {
+        Database.Tables.printDdl
+      }
+
   }
 }
