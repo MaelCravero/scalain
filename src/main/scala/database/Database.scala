d48016e0318c25f68acba954dff28ccca4993e4f
Mael Cravero <mael.cravero@epita.fr>
2020-06-02 16:50:29 +0200

    Document the database package

--- src/main/scala/database/Database.scala
+++ src/main/scala/database/Database.scala
@@ -6,14 +6,17 @@ import org.squeryl._
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.adapters.H2Adapter
 
+/** Database class regrouping all DB setup. */
 object Database {
   Class.forName("org.h2.Driver")
 
+  /** The url of the DB. */
   private val url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
-  private val connection = java.sql.DriverManager.getConnection(url)
 
+  /** Create a connection to the DB and initialize its tables. */
   def start(): org.squeryl.Session = {
-    val session = Session.create(connection, new H2Adapter)
+    val session =
+      Session.create(java.sql.DriverManager.getConnection(url), new H2Adapter)
 
     using(session) {
       Tables.create
@@ -22,11 +25,19 @@ object Database {
     session
   }
 
+  /** Tables class containing the tables of the DB. */
   object Tables extends Schema {
+
+    /** Table for countries. */
     val countries = table[Country]("COUNTRIES")
+
+    /** Table for airports. */
     val airports = table[Airport]("AIRPORTS")
+
+    /** Table for runways. */
     val runways = table[Runway]("RUNWAY")
 
+    /** Display the creation request of the tables. */
     def dump(implicit session: org.squeryl.Session) =
       using(session) {
         Database.Tables.printDdl
