f677f35f4096df7de2c54205c0a207c31aa17be0
Arthur Hugonnot <arthur.hugonnot@epita.fr>
2020-06-02 18:38:24 +0200

    Query handler: implemented the query system (missing function's calls

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,37 @@
 package scalain
 
+import scala.io.StdIn.readLine
+
 object Main extends App {
-  println("Hello, World!")
+
+  // Create databases and parse CSV files
+
+  println("Use one of the following commands")
+
+  while (true) {
+      println("\"query\": display a country's airports and runways")
+      println("\"reports\": display statistics")
+      println("\"quit\": quit the program")
+
+    val entry = readLine
+
+    println(entry)
+
+    if (entry == "query")
+    {
+      println("QUERY") // Handle QUERY
+    }
+    else if (entry == "reports")
+    {
+      println("REPORTS") // Handle REPORTS
+    }
+    else if (entry == "quit")
+    {
+      System.exit(0)
+    }
+    else
+    {
+      println("Not a valid command, please use one of the following:")
+    }
+  }
 }
