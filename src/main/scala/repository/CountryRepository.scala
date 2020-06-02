076d4c14e5a3a2442c8cd22b8905e9df56474f10
Mael Cravero <mael.cravero@epita.fr>
2020-06-02 16:49:52 +0200

    Add airport and runway repositories

--- src/main/scala/repository/CountryRepository.scala
+++ src/main/scala/repository/CountryRepository.scala
@@ -5,8 +5,10 @@ import scalain.database.Country
 
 import org.squeryl.PrimitiveTypeMode._
 
+/** CountryRepository class managing requests to the DB. */
 class CountryRepository(implicit val session: org.squeryl.Session) {
-  def putCountry(country: Country) {
+  /** Add a new country to the the COUNTRIES table. */
+  def postCountry(country: Country) {
     using(session) {
       Database.Tables.countries.insert(country)
     }
