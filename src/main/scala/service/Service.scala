daaa23fd2d18de86351dbd950084291e34e6ebc4
Arthur Hugonnot <arthur.hugonnot@epita.fr>
2020-06-03 23:33:05 +0200

    Service: add getMostCommonRunwayLatitudes

--- src/main/scala/service/Service.scala
+++ src/main/scala/service/Service.scala
@@ -17,8 +17,30 @@ class Service(implicit val session: org.squeryl.Session) {
     val runways = runwayRepo.getAllRunways
 
     val runwaysTypesPerCountryCodes =
-      airports.map({airport =>
-        (airport.isoCountry, runways.filter(runway => runway.airportRef == airport.id))
+      airports.map({ airport =>
+        (airport.isoCountry, runways.filter(runway => runway.airportRef == airport.id)
+                                    .map(runway => runway.surface))
       })
+
+    val runwaysTypesPerCountry =
+      countries.map({ country =>
+        (country.name, runwaysTypesPerCountryCodes.filter(tup => tup._1 == country.code)
+                                                  .flatMap(tup => tup._2))
+      })
+
+    runwaysTypesPerCountry.foreach({ tup =>
+      print(tup._1.concat(":"))
+      tup._2.foreach(surface => print(surface.concat(" ")))
+      print("\n")
+    })
+  }
+
+  def getMostCommonRunwayLatitudes() : List[(String, Int)] = {
+    runwayRepo.getAllRunways
+              .filter(runway => runway.leIdent != None)
+              .groupBy(_.leIdent)
+              .map(tup => (tup._1.get, tup._2.length)).toList
+              .sortWith((el1, el2) => el1._2 > el2._2)
+              .take(10)
   }
 }
