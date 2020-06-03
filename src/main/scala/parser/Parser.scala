b7b5f63cdc3e1423268831e09622f41028202df5
Arthur Hugonnot <arthur.hugonnot@epita.fr>
2020-06-03 19:59:55 +0200

    Parser: documented code

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -14,25 +14,28 @@ object Parser {
     source.getLines
   }
 
-
+  /** Parse a CSV file from a path **/
   def parse_resource(path : String) = {
     val lines = read_file(path)
     lines.next
     lines.map(line => line.split(','))
   }
 
+  /** Parse the airports CSV file to a collection of Airport **/
   def parse_airports() = {
     parse_resource("resources/airports.csv").map({ array =>
       new Airport(array(0), array(3), array(8))
     })
   }
 
+  /** Parse the countries CSV file to a collection of Country **/
   def parse_countries() = {
     parse_resource("resources/countries.csv").map({ array =>
       new Country(array(1), array(2), if (array.length <= 5) None else Some(array(5)))
     })
   }
 
+  /** Parse the runways CSV file to a collection of Runway **/
   def parse_runways() = {
     var c = 0
     parse_resource("resources/runways.csv").map({ array =>
@@ -40,6 +43,7 @@ object Parser {
     })
   }
 
+  /** PArse the 3 CSV files and add the datas into the database **/
   def include_resources(session : org.squeryl.Session) : Unit = {
     using (session) {
       parse_airports().foreach(Database.Tables.airports.insert)
