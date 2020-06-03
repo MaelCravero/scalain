88ce3555005c19323eac39f26f82b24092fe5fc0
Mael Cravero <mael.cravero@epita.fr>
2020-06-03 23:42:55 +0200

    Refacto main function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,22 +3,23 @@ package scalain
 import scala.io.StdIn.readLine
 import scalain.parser._
 import scalain.database.Database
-import scalain.service._
+import scalain.controller.Controller
 import org.squeryl.PrimitiveTypeMode._
 
 object Main extends App {
 
   /** Start database **/
   implicit val session = Database.start()
+
   /** Parse the resources files **/
+  print("Parsing resources...")
   Parser.include_resources(session)
+  println(" Done.")
 
   /** Instantiate the services **/
-  val service = new Service
+  val controller = new Controller
 
-  println("Use one of the following commands")
-
-  while (true) {
+  def parseUserInput() {
     println("\"query\": display a country's airports and runways")
     println("\"reports\": display statistics")
     println("\"quit\": quit the program")
@@ -26,24 +27,33 @@ object Main extends App {
     /** Read input on stdin **/
     val entry = readLine()
 
-    if (entry == "query")
-    {
-      println("QUERY") // Handle QUERY
-    }
-    else if (entry == "reports")
-    {
-      service.displayTypeRunwayPerCountry()
-      println("REPORTS") // Handle REPORTS
-    }
-    else if (entry == "quit")
-    {
-      System.exit(0)
-    }
-    else
-    {
-      println("Not a valid command, please use one of the following:")
+    entry match {
+      case "query" => {
+        queryInput()
+        parseUserInput()
+      }
+      case "reports" => {
+        printReports()
+        parseUserInput()
+      }
+      case "quit" => println("Exiting.")
+      case _ => {
+        println("Not a valid command, please use one of the following:")
+        parseUserInput()
+      }
     }
   }
+
+  def queryInput() {
+    println("QUERY")
+  }
+  def printReports() {
+    controller.displayRunwayTypePerCountry
+  }
+
+  println("Use one of the following commands")
+
+  parseUserInput()
 
   session.close
 }
